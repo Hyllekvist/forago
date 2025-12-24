0e3a2324466ea36c62fa689e80e0e2b5443a81e8
Hyllekvist <42335892+Hyllekvist@users.noreply.github.com>
2025-12-24 06:39:40 +0100

    Update Card.tsx

--- src/components/UI/Card.tsx
+++ src/components/UI/Card.tsx
@@ -1,5 +1,15 @@
+import clsx from "clsx";
 import styles from "./Card.module.css";
 
-export function Card({ children, className }: { children: React.ReactNode; className?: string }) {
-  return <div className={`${styles.card} ${className ?? ""}`}>{children}</div>;
+type Props = {
+  children: React.ReactNode;
+  className?: string;
+};
+
+export function Card({ children, className }: Props) {
+  return (
+    <div className={clsx(styles.card, className)}>
+      {children}
+    </div>
+  );
 }
