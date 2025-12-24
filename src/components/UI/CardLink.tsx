a2f0a5f296fa68bb7d7d8eb132cbf99cdc45172f
Hyllekvist <42335892+Hyllekvist@users.noreply.github.com>
2025-12-24 06:45:28 +0100

    Update CardLink.tsx

--- src/components/UI/CardLink.tsx
+++ src/components/UI/CardLink.tsx
@@ -1,18 +1,18 @@
 import Link from "next/link";
-import clsx from "clsx";
 import styles from "./CardLink.module.css";
 
-export function CardLink({
-  href,
-  children,
-  className,
-}: {
+type Props = {
   href: string;
   children: React.ReactNode;
   className?: string;
-}) {
+};
+
+export function CardLink({ href, children, className }: Props) {
   return (
-    <Link href={href} className={clsx(styles.card, className)}>
+    <Link
+      href={href}
+      className={`${styles.card}${className ? ` ${className}` : ""}`}
+    >
       {children}
     </Link>
   );
