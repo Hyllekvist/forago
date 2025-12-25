aff7b113c28b7ce19d09cea0400227c8653caae6
Hyllekvist <42335892+Hyllekvist@users.noreply.github.com>
2025-12-25 22:10:54 +0100

    Update supercluster.d.ts

--- src/types/supercluster.d.ts
+++ src/types/supercluster.d.ts
@@ -1,1 +1,26 @@
-declare module "supercluster";
+declare module "supercluster" {
+  export type Feature<P = any> = {
+    type: "Feature";
+    properties: P;
+    geometry: { type: "Point"; coordinates: [number, number] };
+  };
+
+  export type Options = {
+    radius?: number;
+    maxZoom?: number;
+    minZoom?: number;
+    extent?: number;
+    nodeSize?: number;
+    log?: boolean;
+  };
+
+  export default class Supercluster<P = any> {
+    constructor(options?: Options);
+    load(points: Feature<P>[]): this;
+    getClusters(
+      bbox: [number, number, number, number],
+      zoom: number
+    ): Array<any>;
+    getClusterExpansionZoom(clusterId: number): number;
+  }
+}
