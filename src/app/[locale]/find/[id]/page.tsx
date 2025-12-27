d2d4481afe5529caa223617423b330cdf8afe29e
Hyllekvist <42335892+Hyllekvist@users.noreply.github.com>
2025-12-27 07:42:46 +0100

    Update page.tsx

--- src/app/[locale]/find/[id]/page.tsx
+++ src/app/[locale]/find/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { supabaseServer } from "@/lib/supabase/server";
+import { supabaseServer } from "@/lib/supabase/server"; 
 import FindClient from "./FindClient";
 import styles from "./FindPage.module.css";
 
