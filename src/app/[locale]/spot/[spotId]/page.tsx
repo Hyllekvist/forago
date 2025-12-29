6ac45fbd2a7915075502b917afeb44c394280774
Hyllekvist <42335892+Hyllekvist@users.noreply.github.com>
2025-12-29 11:20:05 +0100

    Update page.tsx

--- src/app/[locale]/spot/[spotId]/page.tsx
+++ src/app/[locale]/spot/[spotId]/page.tsx
@@ -1,4 +1,4 @@
-import { notFound } from "next/navigation";
+import { notFound } from "next/navigation"; 
 import Link from "next/link";
 import { supabaseServer } from "@/lib/supabase/server";
 import SpotLogCTA from "./SpotLogCTA";
