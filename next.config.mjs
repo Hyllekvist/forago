864c34d8f00a71578a268232ad53ed49b417d714
Hyllekvist <42335892+Hyllekvist@users.noreply.github.com>
2025-12-31 01:36:55 +0100

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  poweredByHeader: false,
+
   experimental: {
-    typedRoutes: false
+    typedRoutes: false,
   },
+
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "**.supabase.co" }
-    ]
-  }
+      {
+        protocol: "https",
+        hostname: "hycbxiqfeunpzbgcasfo.supabase.co",
+        pathname: "/storage/v1/object/public/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
